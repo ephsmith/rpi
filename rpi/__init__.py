c409c034cc39383e2e4fadca43bbf84e64aff98a
Forrest Smith <fasmith0229@gmail.com>
2018-05-06 13:38:49 +0000

    Added basic package structure for future files.

---
+++ rpi/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# Author: Forrest Smith
+from . import ussensor
---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Author: Forrest Smith
+from setuptools import setup
+
+
+setup(name='rpi',
+      version='0.1',
+      description='A small collection of raspberrypi related modules',
+      url='https://github.com/ephsmith/rpi',
+      author='Forrest Smith',
+      author_email='fasmith0229@gmail.com',
+      license='MIT',
+      packages=['rpi'],
+      zip_safe=False)
