5a17bc933e98305820c8f8a49326bc517fb9d308
Forrest Smith <fasmith0229@gmail.com>
2018-07-17 16:46:02 -0500

    added pose saves and pose combo box to the gui

--- examples/arm/arm_gui.py
+++ examples/arm/arm_gui.py
@@ -1,14 +1,52 @@
 # -*- coding: utf-8 -*-
 # Author: Forrest Smith
 from guizero import App, Text, PushButton, Slider
+from guizero import TextBox, Combo
 
+home = {k: 50 for k in ('base', 'shoulder',
+                        'elbow', 'wrist',
+                        'gripper')
+        }
+
+poses = {'home': home}
 
 app = App(title='Sliders', layout='grid')
+
+def get_pose():
+    return {k: v for k, v in zip(('base', 'shoulder',
+                                  'elbow', 'wrist', 'gripper'),
+                                 (base.value, shoulder.value, elbow.value,
+                                  wrist.value, gripper.value))}
+
+
+def save_pose():
+    # print('Pose to save: {}\n{}'.format(pose_name.value, get_pose()))
+    poses[pose_name.value] = get_pose()
+    pose_combo.append(pose_name.value)
+    pose_name.clear()
+
+
+def update_sliders(key):
+    if key == '-':
+        return
+    else:
+        d = poses[key]
+        base.value = d['base']
+        shoulder.value = d['shoulder']
+        elbow.value = d['elbow']
+        wrist.value = d['wrist']
+        gripper.value = d['gripper']
+
+
 base = Slider(app, grid=[1, 0], start=1, end=100)
 shoulder = Slider(app, grid=[1, 1], start=1, end=100)
 elbow = Slider(app, grid=[1, 2], start=1, end=100)
 wrist = Slider(app, grid=[1, 3], start=1, end=100)
 gripper = Slider(app, grid=[1, 4], start=1, end=100)
+
+for s in (base, shoulder, elbow, wrist, gripper):
+    s.value = 50
+
 
 base_text = Text(app, text='Base',
                  grid=[0, 0], align='left')
@@ -20,7 +58,17 @@ wrist_text = Text(app, text='Wrist',
                   grid=[0, 3], align='left')
 gripper_text = Text(app, text='Gripper',
                     grid=[0, 4], align='left')
+
 move_button = PushButton(app, text='Move',
                          grid=[1, 5])
+
+pose_name = TextBox(app, grid=[0, 6])
+
+pose_combo = Combo(app, options=['home'], grid=[2, 0], command=update_sliders)
+pose_combo.width = 30
+
+
+save_button = PushButton(app, text='Save Pose',
+                         grid=[0, 7], command=save_pose)
 
 app.display()
